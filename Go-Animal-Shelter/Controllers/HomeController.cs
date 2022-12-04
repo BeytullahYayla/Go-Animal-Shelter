3f48bd571dd2f97d47e5f7cae848dabfa2e9b6f2
Beytullah Yayla <beytullah.yayla@ogr.sakarya.edu.tr>
2022-12-04 20:12:28 +0300

    ProjectCreated

---
+++ Go-Animal-Shelter/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Go_Animal_Shelter.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Go_Animal_Shelter.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+       
+    }
+}
---
+++ Go-Animal-Shelter/Controllers/VeterinerianController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Go_Animal_Shelter.Controllers
+{
+	public class VeterinerianController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
