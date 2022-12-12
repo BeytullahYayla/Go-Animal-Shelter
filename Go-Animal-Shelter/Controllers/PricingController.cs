2a5cd5b9bcb4315ab39662da24cf6adef7bce7e3
Beytullah Yayla <beytullah.yayla@ogr.sakarya.edu.tr>
2022-12-12 15:15:30 +0300

    Frontend Created

---
+++ Go-Animal-Shelter/Controllers/PricingController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Go_Animal_Shelter.Controllers
+{
+	public class PricingController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
---
+++ Go-Animal-Shelter/Controllers/ServicesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Go_Animal_Shelter.Controllers
+{
+	public class ServicesController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
