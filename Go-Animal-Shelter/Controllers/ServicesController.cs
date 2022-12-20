1d1749af80e4cb6cd64e6459c4a5b5b4328a6d17
Beytullah Yayla <beytullah.yayla@ogr.sakarya.edu.tr>
2022-12-20 13:40:21 +0300

    Css Error Solved

--- Go-Animal-Shelter/Controllers/ServicesController.cs
+++ Go-Animal-Shelter/Controllers/ServicesController.cs
@@ -1,13 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Go_Animal_Shelter.Controllers
 {
 	public class ServicesController : Controller
 	{
+		ServiceManager serviceManager=new ServiceManager(new EfServiceDal());
 		public IActionResult Index()
 		{
+			var results = serviceManager.GetAll();
+			return View(results.Data);
+		}
 
-			return View();
+		
+		public IActionResult GetAll()
+		{
+			var results=serviceManager.GetAll()
+;			return View(results.Data);
 		}
 	}
 }
