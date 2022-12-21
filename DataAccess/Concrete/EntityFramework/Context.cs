b98f18b49dbb6cd836899a5150046ca6c502df2a
Sercan <ssercanozerr@gmail.com>
2022-12-21 18:37:22 +0300

    düzenleme

--- DataAccess/Concrete/EntityFramework/Context.cs
+++ DataAccess/Concrete/EntityFramework/Context.cs
@@ -13,14 +13,17 @@ namespace DataAccess.Concrete.EntityFramework
 	{
 		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 		{
-			optionsBuilder.UseNpgsql(@"Host=localhost;Database=AnimalShelter;Username=postgres;Password=Beytullah.123");
+			optionsBuilder.UseNpgsql(@"Host=localhost;Database=AnimalShelter;Username=postgres;Password=birdendokuza");
 		}
 		public DbSet<Service> Services { get; set; }
 		public DbSet<AnimalCategory> AnimalCategories { get; set; }
 		public DbSet<Contact> Contacts { get; set; }
 		public DbSet<Species> Species { get; set; }
-		public DbSet<Veterinerian> Veterersians { get; set;}
+		public DbSet<Veterinerian> Veterinerians { get; set;}
 		public DbSet<Pet> Pets { get; set; }
+		public DbSet<Roles> Roles { get; set; }
+		public DbSet<User> Users { get; set; }
+		public DbSet<PetOwner> PetsOwners { get; set; }
 	}
 }
 
