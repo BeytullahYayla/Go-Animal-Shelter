7f2e50e870b786d410ba3438b977f15267668fe8
Beytullah Yayla <beytullah.yayla@ogr.sakarya.edu.tr>
2022-12-21 17:02:07 +0300

    CRUD view created for animalcategory

--- Go-Animal-Shelter/Controllers/AdminController.cs
+++ Go-Animal-Shelter/Controllers/AdminController.cs
@@ -1,12 +1,64 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Go_Animal_Shelter.Controllers
 {
 	public class AdminController : Controller
 	{
+		AnimalCategoryManager animalCategoryManager = new AnimalCategoryManager(new EfAnimalCategoryDal());
 		public IActionResult Index()
 		{
 			return View();
 		}
+
+		[HttpGet]
+		public IActionResult List()
+		{
+			var result=animalCategoryManager.GetAll();
+			if (result.Success)
+			{
+                return View(result.Data);
+            }
+			return View("Index");
+			
+		}
+
+		public IActionResult AddAnimalCategory()
+		{
+			return View();
+		}
+		[HttpPost]
+		public IActionResult AddAnimalCategory(AnimalCategory animalCategory)
+		{
+			var result=animalCategoryManager.Add(animalCategory);
+			if (result.Success)
+			{
+				return RedirectToAction("List");
+			}
+			return View();
+			
+		}
+
+		public IActionResult Update()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult Update(AnimalCategory animalCategory)
+		{
+			var result=animalCategoryManager.Update(animalCategory);
+			if (result.Success)
+			{
+				return RedirectToAction("List");
+			}
+			return View();
+
+		}
+
+
 	}
 }
